e2e54e7a9075d11b55a417030703d6f5676b6dc9
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2025-02-16 17:21:56 +0300

    add legal warnings for footer

--- app/components/home/footer.tsx
+++ app/components/home/footer.tsx
@@ -84,7 +84,7 @@ export const Footer = () => {
   return (
     <div className="w-full px-6 py-20">
       <div className="border-white/4 border-y-[1px] px-0 py-10 sm:px-8 md:px-24">
-        <div className="flex flex-col gap-16 sm:gap-8 px-4 py-6 sm:flex-row">
+        <div className="flex flex-col gap-16 px-4 py-6 sm:flex-row sm:gap-8">
           <Link to="/" className="flex flex-1 justify-start">
             <svg
               width={105}
@@ -100,8 +100,31 @@ export const Footer = () => {
           ))}
         </div>
       </div>
-      <div className="px-24">
-        <div>Legal Stuff</div>
+      <div className="px-0 sm:px-8 md:px-24">
+        <div className="flex flex-col gap-4 px-4 py-6">
+          <p className="font-sans text-[#AFC9DE] text-xs leading-6">
+            © 2025 BuCode. All rights reserved
+          </p>
+          <p className="max-w-5xl font-sans text-[#7494AF] text-xs leading-6">
+            BuCode and its organizing committee strive to provide the most
+            accurate and up-to-date information regarding the competition on
+            this website. However, we reserve the right to make adjustments to
+            the competition rules, schedule, and other related content without
+            prior notice, as deemed necessary for the smooth conduct and
+            integrity of the event.
+            <br />
+            BuCode is not responsible for any inconvenience or loss caused by
+            such changes. All decisions made by the competition judges and
+            organizers are final.
+            <br />
+            <br />
+            By participating in BuCode, competitors agree to abide by all the
+            rules and regulations set forth by the organizers. BuCode respects
+            your privacy and is committed to protecting your personal
+            information. For more details on our privacy policy and terms of
+            use, please visit Data Privacy.
+          </p>
+        </div>
       </div>
     </div>
   );
