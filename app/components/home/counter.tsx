5f31f47cfea99cd94290de109b4218c250591e08
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2025-02-20 17:16:59 +0300

    add digits to counter section

--- app/components/home/counter.tsx
+++ app/components/home/counter.tsx
@@ -41,8 +41,35 @@ const rectangleClass = cva("h-full w-[2px]", {
   },
 });
 
+const CounterDigit = ({
+  digit,
+  timeUnit,
+}: { digit: number; timeUnit: string }) => {
+  return (
+    <div className="flex w-[120px] flex-col items-center">
+      <div className="font-sans font-semibold text-[#C9E9FF] text-[64px] leading-18 tracking-[-0.01em]">
+        {digit}
+      </div>
+      <div className="font-medium font-sans text-[#7494AF] text-base leading-6">
+        {timeUnit}
+      </div>
+    </div>
+  );
+};
+
+const DigitSeperator = ({ visible }: { visible: boolean }) => {
+  return (
+    <div
+      className={`flex h-full w-[80px] flex-col items-center justify-center${visible ? "" : " hidden md:flex"}`}
+    >
+      <div className="h-12 w-[1px] bg-[radial-gradient(circle_at_center,_rgb(99,140,164,0.64),_rgb(99,140,164,0.0))]" />
+    </div>
+  );
+};
+
 export const Counter = () => {
   const [activeTab, setActiveTab] = useState<string>("qualification");
+  console.log(activeTab);
 
   return (
     <div className="w-dvg p-4">
@@ -93,15 +120,18 @@ export const Counter = () => {
                 </h1>
                 <div className="h-[1px] flex-1 bg-[linear-gradient(to_right,_rgb(99,140,164,0.64),_rgb(99,140,164,0.0))]" />
               </div>
-              <p className="max-w-[720px]">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo
-                voluptates cum officia suscipit architecto rerum mollitia vitae
-                quas! Temporibus assumenda, est aspernatur ipsum optio adipisci
-                a autem qui beatae consequatur?
-              </p>
+              <div className="grid grid-cols-3 place-items-center gap-y-12 md:grid-cols-7 md:gap-y-0">
+                <CounterDigit digit={19} timeUnit="days" />
+                <DigitSeperator visible={true} />
+                <CounterDigit digit={5} timeUnit="hours" />
+                <DigitSeperator visible={false} />
+                <CounterDigit digit={7} timeUnit="minutes" />
+                <DigitSeperator visible={true} />
+                <CounterDigit digit={32} timeUnit="seconds" />
+              </div>
               <div className="flex w-full items-center gap-3">
                 <div className="h-[1px] flex-1 bg-[linear-gradient(to_left,_rgb(99,140,164,0.64),_rgb(99,140,164,0.0))]" />
-                <h1 className="pb-1 font-bold font-display text-[#446377] text-sm uppercase leading-5 tracking-4xl">
+                <h1 className="relative pb-1 font-bold font-display text-[#446377] text-sm uppercase leading-5 tracking-4xl before:absolute before:bottom-0 before:h-[1px] before:w-full before:bg-[linear-gradient(to_left,_rgb(99,140,164,0.64),_rgb(99,140,164,0.0))] before:['']">
                   12:00 PM
                 </h1>
                 <div className="h-[1px] flex-1 bg-[linear-gradient(to_right,_rgb(99,140,164,0.64),_rgb(99,140,164,0.0))]" />
