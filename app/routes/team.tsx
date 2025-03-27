7b2cd4c1aa96186f4a6a6efab23831cf7aa5a616
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2025-03-28 00:21:24 +0300

    update team member information

--- app/routes/team.tsx
+++ app/routes/team.tsx
@@ -50,9 +50,9 @@ export const subteams = [
 // 3: Org
 const team: TeamMember[] = [
   {
-    name: "John Doe",
+    name: "Işıl Su Karakuzu",
     subteam: subteams[0],
-    bio: "John Doe is a software engineer at Google",
+    bio: "Director of BuCode",
     image:
       "https://media.licdn.com/dms/image/v2/D5603AQG7_il61S-hvg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1683663177233?e=1748476800&v=beta&t=9noaOBFCihxB7VvT25TesX_RX_E7uP6bPAn6t9bGYak",
     links: {
@@ -62,9 +62,9 @@ const team: TeamMember[] = [
     },
   },
   {
-    name: "Jane Doe",
-    subteam: subteams[1],
-    bio: "Jane Doe is a software engineer at Google",
+    name: "Tuğra Acar",
+    subteam: subteams[0],
+    bio: "Co-Director of BuCode",
     image: "/images/team/jane-doe.jpg",
     links: {
       github: "https://github.com/jane-doe",
@@ -73,9 +73,9 @@ const team: TeamMember[] = [
     },
   },
   {
-    name: "John Doe",
+    name: "Ümit Can Evleksiz",
     subteam: subteams[2],
-    bio: "John Doe is a software engineer at Google",
+    bio: "Design and Development Lead",
     image: "/images/team/john-doe.jpg",
     links: {
       github: "https://github.com/john-doe",
@@ -84,9 +84,185 @@ const team: TeamMember[] = [
     },
   },
   {
-    name: "John Doe",
+    name: "Özgür Deniz Demir",
+    subteam: subteams[2],
+    bio: "Developer",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Ahmed Faruk Özgür",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Berk Sel",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Atakan Yaşar",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Fatih Solak",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Hakan Gökdoğan",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Şevket Onur Yılmaz",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Utku Çelik",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Yusuf Akın",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Melih Akpınar",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Celaleddin Ömer Sağlam",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Emin Ersüs",
+    subteam: subteams[1],
+    bio: "Problem Setter",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Damla Demirok",
     subteam: subteams[3],
-    bio: "John Doe is a software engineer at Google",
+    bio: "Event Organization",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Nil Zeren Doğan",
+    subteam: subteams[3],
+    bio: "Event Organization",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Umut Efe Seki",
+    subteam: subteams[3],
+    bio: "Sponsorship",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Ömer Taha Örnek",
+    subteam: subteams[3],
+    bio: "Sponsorship",
+    image: "/images/team/john-doe.jpg",
+    links: {
+      github: "https://github.com/john-doe",
+      linkedin: "https://www.linkedin.com/in/john-doe",
+      twitter: undefined,
+    },
+  },
+  {
+    name: "Musa Kaan Güney",
+    subteam: subteams[3],
+    bio: "Event Organization",
     image: "/images/team/john-doe.jpg",
     links: {
       github: "https://github.com/john-doe",
